4daa858b9a2ed4600add8916c478b623ee144151
meghanarao2911 <46216727+meghanarao2911@users.noreply.github.com>
2019-06-18 22:35:38 +0530

    Common Elements in the list

--- Data Structure Based/List/CommonElements.py
+++ Data Structure Based/List/CommonElements.py
@@ -3,12 +3,13 @@ l1=[1,2,3,4,5,6,7]
 l2=[8,9,10,5,6,8,56,9]
 
 def findCommon(l1,l2):
-
+    count=0
     common=[]
     for i in l1:
         if i in l2:
+            count=count+1
             common.append(i)
-    return common
+    return common,count
 
-c=findCommon(l1,l2)
-print("Common elements",c)
+c,cu=findCommon(l1,l2)
+print("Common of elements",c,"are",cu)
